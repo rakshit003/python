89a0906e6fd26d2b22188c252bed70a961e19aa3
rakshit003 <80508581+rakshit003@users.noreply.github.com>
2021-07-19 16:58:21 +0530

    Create config.py

---
+++ ML Project Default/src/config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# config.py
+"""
+Created on Mon Jul 19 14:08:30 2021
+
+@author: rchauhan32
+
+This is config file that can control the input and output file locations
+
+"""
+
+RAW_FILE_TRAIN= r"C:\Users\rakshit\Documents\research\datasets\mnist\mnist_train.csv"
+RAW_FILE_TEST=  r"C:\Users\rakshit\Documents\research\datasets\mnist\mnist_test.csv"
+
+TRAINING_FILE= r"C:\Users\rakshit\Documents\research\datasets\mnist\mnist_train_folds.csv"
+
+MODEL_OUTPUT= "../models/"
